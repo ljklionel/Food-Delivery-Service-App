817577f1269f5fbd6875e1d6049f55e95d55db3f
linyutinglyt <e0332737@u.nus.edu>
2020-03-19 22:40:40 +0800

    Add Users and FDSManagers database code

---
+++ backend/init_user.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+	username VARCHAR(64) PRIMARY KEY,
+	hashedPassword VARCHAR(128) NOT NULL,
+	phoneNumber NUMERIC(16,0) NOT NULL,
+	name VARCHAR(64) NOT NULL
+);
+
+DROP TABLE IF EXISTS FDSManagers;
+CREATE TABLE FDSManagers (
+	username VARCHAR(64) PRIMARY KEY REFERENCES Users ON DELETE CASCADE
+)
+
+DROP TABLE IF EXISTS FDSPromotions;
+CREATE TABLE FDSPromotions (
+    promoId VARCHAR(64),
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    discount NUMERIC(4,2) NOT NULL,
+	promoDescription VARCHAR(64),
+	managerUsername VARCHAR(64) REFERENCES FDSManagers,
+    PRIMARY KEY (promoId, rname)
+);
