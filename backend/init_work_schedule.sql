5769260e1c40c9e3f0c087d8d6474f364353dea5
ljklionel <lionlim97@hotmail.com>
2020-03-20 23:02:26 +0800

    Fix init_work_schedule.sql

--- backend/init_work_schedule.sql
+++ backend/init_work_schedule.sql
@@ -1,22 +1,22 @@
-DROP TABLE IF EXISTS Riders;
+DROP TABLE IF EXISTS DeliveryRiders;
 CREATE TABLE Riders (
     username VARCHAR(64) PRIMARY KEY REFERENCES Users ON DELETE CASCADE, 
     salary INTEGER;
 );
 
-DROP TABLE IF EXISTS partTimers;
-CREATE TABLE partTimers (
+DROP TABLE IF EXISTS PartTimers;
+CREATE TABLE PartTimers (
     username VARCHAR(64) PRIMARY KEY REFERENCES Riders ON DELETE CASCADE,
     workHours INTEGER,
 );
 
-DROP TABLE IF EXISTS fullTimers;
-CREATE TABLE fullTimers (
+DROP TABLE IF EXISTS FullTimers;
+CREATE TABLE FullTimers (
     username VARCHAR(64) PRIMARY KEY REFERENCES Riders ON DELETE CASCADE,
 );
 
-DROP TABLE IF EXISTS partTimeShifts;
-CREATE TABLE partTimeShifts (
+DROP TABLE IF EXISTS PartTimeShifts;
+CREATE TABLE PartTimeShifts (
     workDay INTEGER,
     startHour INTEGER,
     endHour INTEGER,
@@ -24,8 +24,8 @@ CREATE TABLE partTimeShifts (
     PRIMARY KEY (startHour, endHour, workDay),
 );
 
-DROP TABLE IF EXISTS fullTimeShifts;
-CREATE TABLE fullTimeShifts (
+DROP TABLE IF EXISTS FullTimeShifts;
+CREATE TABLE FullTimeShifts (
     workDay INTEGER,
     startHour INTEGER,
     endHour INTEGER,
@@ -33,30 +33,30 @@ CREATE TABLE fullTimeShifts (
     PRIMARY KEY (workDay, startHour, endHour),
 );
 
-DROP TABLE IF EXISTS monthlyWorkSched;
-CREATE TABLE monthlyWorkSched (
-    username VARCHAR(64) REFERENCES fullTimers ON DELETE CASCADE,
-    workDay INTEGER REFERENCES fullTimeShifts,
-    startHour INTEGER REFERENCES fullTimeShifts,
-    endHour INTEGER REFERENCES fullTimeShifts,
+DROP TABLE IF EXISTS MonthlyWorkSched;
+CREATE TABLE MonthlyWorkSched (
+    username VARCHAR(64) REFERENCES FullTimers ON DELETE CASCADE,
+    workDay INTEGER REFERENCES FullTimeShifts,
+    startHour INTEGER REFERENCES FullTimeShifts,
+    endHour INTEGER REFERENCES FullTimeShifts,
     PRIMARY KEY (username, workDay, startHour, endHour),
 );
 
-DROP TABLE IF EXISTS weeklyWorkSched;
-CREATE TABLE weeklyWorkSched (
-    username VARCHAR(64) REFERENCES partTimers ON DELETE CASCADE,
-    workDay INTEGER REFERENCES partTimeShifts,
-    startHour INTEGER REFERENCES partTimeShifts,
-    endHour INTEGER REFERENCES partTimeShifts,
+DROP TABLE IF EXISTS WeeklyWorkSched;
+CREATE TABLE WeeklyWorkSched (
+    username VARCHAR(64) REFERENCES PartTimers ON DELETE CASCADE,
+    workDay INTEGER REFERENCES PartTimeShifts,
+    startHour INTEGER REFERENCES PartTimeShifts,
+    endHour INTEGER REFERENCES PartTimeShifts,
     PRIMARY KEY (username, workDay, startHour, endHour),
 );
 
-CREATE OR REPLACE FUNCTION checkTotalHours () RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_total_hours_trigger () RETURNS TRIGGER AS $$
 DECLARE
     totalHours INTEGER;
 BEGIN
     SELECT PT.workHours INTO totalHours
-        FROM partTimers PT
+        FROM PartTimers PT
         WHERE PT.username = NEW.username
     IF totalHours > 48 OR totalHours < 10 THEN
         RAISE EXCEPTION '% cannot work for more than 48 hours or less than 10 hours per week', NEW.username;
@@ -65,17 +65,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION updateHoursPartTime () RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION update_hours_part_time () RETURNS TRIGGER AS $$
 DECLARE
     totalHour INTEGER
 BEGIN
     SELECT PTS.workHours INTO totalHour
-        FROM partTimeShifts PTS
+        FROM PartTimeShifts PTS
         WHERE PTS.workDay = NEW.workDay
         AND PTS.startHour = NEW.startHour
         AND PTS.endHour = NEW.endHour
     BEGIN
-        UPDATE partTimers
+        UPDATE PartTimers
         SET workHours = workHours + totalHour,
         WHERE username = NEW.username;
     COMMIT;
@@ -83,12 +83,12 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION partTimeBreakCheck() RETURN TRIGGER AS $$
+CREATE OR REPLACE FUNCTION part_time_break_check() RETURN TRIGGER AS $$
 DECLARE
     endHour INTEGER
 BEGIN
     SELECT WWS.endHour INTO endHour
-        FROM weeklyWorkSched WWS
+        FROM WeeklyWorkSched WWS
         WHERE WWS.username = NEW.username
     IF endHour = NEW.startHour THEN
         RAISE EXCEPTION '% does not have an hour break between 2 cosecutive interval', NEW.username;
@@ -97,24 +97,24 @@ BEGIN
 END;
 $$LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS partTimeBreakCheck ON weeklyWorkSched CASCADE;
-CREATE CONSTRAINT TRIGGER partTimeBreakCheck
-    AFTER UPDATE OF username, workDay,startHour, endHour OR INSERT ON weeklyWorkSched
+DROP TRIGGER IF EXISTS part_time_break_check ON WeeklyWorkSched CASCADE;
+CREATE CONSTRAINT TRIGGER part_time_break_check
+    AFTER UPDATE OF username, workDay,startHour, endHour OR INSERT ON WeeklyWorkSched
     DEFERRABLE INITIALLY DEFERED
     FOR EACH ROW
-    EXECUTE FUNCTION partTimeBreakCheck();
+    EXECUTE FUNCTION part_time_break_check();
 
-DROP TRIGGER IF EXISTS updateHoursPartTime ON weeklyWorkSched CASCADE;
-CREATE CONSTRAINT TRIGGER updateHoursPartTime
-    AFTER UPDATE OF username, workDay,startHour, endHour OR INSERT ON weeklyWorkSched
+DROP TRIGGER IF EXISTS update_hours_part_time ON WeeklyWorkSched CASCADE;
+CREATE CONSTRAINT TRIGGER update_hours_part_time
+    AFTER UPDATE OF username, workDay,startHour, endHour OR INSERT ON WeeklyWorkSched
     DEFERRABLE INITIALLY DEFERED
     FOR EACH ROW
-    EXECUTE FUNCTION updateHoursPartTime();
+    EXECUTE FUNCTION update_hours_part_time();
 
-DROP TRIGGER IF EXISTS checkTotalHours ON partTimers CASCADE;
-CREATE CONSTRAINT TRIGGER updateHoursPartTime
-    AFTER UPDATE OF username, workHours ON partTimers
+DROP TRIGGER IF EXISTS check_total_hours_trigger ON PartTimers CASCADE;
+CREATE CONSTRAINT TRIGGER check_total_hours_trigger
+    AFTER UPDATE OF username, workHours ON PartTimers
     DEFERRABLE INITIALLY DEFERED
     FOR EACH ROW
-    EXECUTE FUNCTION updateHoursPartTime();
+    EXECUTE FUNCTION check_total_hours_trigger();
     
